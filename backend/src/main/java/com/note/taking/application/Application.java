c1aa809590f41a408d3d477d35bd2f02087592a1
Jacky Schoen <jschoen@hotmail.nl>
2024-08-10 18:08:01 +0200

    added basic java backend files to the project

---
+++ backend/src/main/java/com/note/taking/application/Application.java
@@ -0,0 +1,15 @@
+package com.note.taking.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	/**
+	 * Runs the Spring Boot application
+	 * @param args the application arguments.
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
