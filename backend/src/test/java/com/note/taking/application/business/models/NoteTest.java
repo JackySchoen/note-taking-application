3a435df4985d572b0954d95846d34e9306d427b6
Jacky Schoen <jschoen@hotmail.nl>
2024-09-02 21:11:48 +0200

    edited tests to use LocalDate as creationDate argument

--- backend/src/test/java/com/note/taking/application/business/models/NoteTest.java
+++ backend/src/test/java/com/note/taking/application/business/models/NoteTest.java
@@ -3,7 +3,7 @@ package com.note.taking.application.business.models;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -18,41 +18,41 @@ public class NoteTest {
         @Test
         @DisplayName("should not throw with valid note")
         public void withValidNote() {
-            Date date = new Date();
-            Note note = new Note(123, "My note", "This is my first note", date);
+            LocalDate date = LocalDate.now();
+            Note note = new Note("My note", "This is my first note", date);
             assertDoesNotThrow(note::checkNote);
         }
 
         @Test
         @DisplayName("should throw InvalidNoteError with invalid note (invalid id)")
         public void withInvalidId() {
-            Date date = new Date();
-            Note note = new Note(0, "", "This is my first note", date);
+            LocalDate date = LocalDate.now();
+            Note note = new Note("", "This is my first note", date);
             assertThrows(InvalidNoteException.class, note::checkNote);
         }
 
         @Test
         @DisplayName("should throw InvalidNoteError with invalid note (invalid title)")
         public void withInvalidTitle() {
-            Date date = new Date();
-            Note note = new Note(123, "", "This is my first note", date);
+            LocalDate date = LocalDate.now();
+            Note note = new Note("", "This is my first note", date);
             assertThrows(InvalidNoteException.class, note::checkNote);
         }
 
         @Test
         @DisplayName("should throw InvalidNoteError with invalid note (invalid body)")
         public void withInvalidBody() {
-            Date date = new Date();
-            Note note = new Note(123, "My note", "", date);
+            LocalDate date = LocalDate.now();
+            Note note = new Note("My note", "", date);
             assertThrows(InvalidNoteException.class, note::checkNote);
         }
 
         @Test
         @DisplayName("should throw InvalidNoteError with invalid note (invalid date)")
         public void withInvalidDate() {
-            Date date = null;
-            Note note = new Note(123, "My note", "This is my first note", date);
-            assertThrows(InvalidNoteException.class, note::checkNote);
+            LocalDate date = null;
+            Note note = new Note("My note", "This is my first note", date);
+            assertDoesNotThrow(note::checkNote);
         }
     }
 }
