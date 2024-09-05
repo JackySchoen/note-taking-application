01e2d39bd522959a6c5995ca0af7e517cd9addb7
Jacky Schoen <jschoen@hotmail.nl>
2024-09-05 20:40:44 +0200

    added checkId method to NoteService

--- backend/src/main/java/com/note/taking/application/business/services/NoteService.java
+++ backend/src/main/java/com/note/taking/application/business/services/NoteService.java
@@ -1,13 +1,13 @@
 package com.note.taking.application.business.services;
 
-import com.note.taking.application.util.NoteNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.note.taking.application.business.models.Note;
 import com.note.taking.application.data.entities.NoteEntity;
 import com.note.taking.application.data.repositories.NoteRepository;
+import com.note.taking.application.util.InvalidIdException;
 import com.note.taking.application.util.InvalidNoteException;
+import com.note.taking.application.util.NoteNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
@@ -18,6 +18,7 @@ public class NoteService {
 
     /**
      * Saves a new note to storage.
+     *
      * @param note the current note object.
      * @throws InvalidNoteException when a property of the current note object is invalid.
      */
@@ -29,13 +30,29 @@ public class NoteService {
 
     /**
      * Gets an existing note from storage by its id.
+     *
      * @param id the id of the note you want to get from storage.
      * @return the note if one is found by using the provided id.
      * @throws NoteNotFoundException when no note is found by using the provided id.
+     * @throws InvalidIdException    when the id is either below 0 or above the max integer value.
      */
-    public Note getNoteById(int id) throws NoteNotFoundException {
+    public Note getNoteById(int id) throws NoteNotFoundException, InvalidIdException {
+        checkId(id);
         Optional<NoteEntity> noteFromStorage = noteRepository.findById(id);
-        if (noteFromStorage.isEmpty()) throw new NoteNotFoundException("note with id \"" + id + "\" could not be found");
+        if (noteFromStorage.isEmpty())
+            throw new NoteNotFoundException("note with id \"" + id + "\" could not be found");
         return new Note(noteFromStorage.get());
+    }
+
+    /**
+     * Checks if id is either below 0, or above integer max value.
+     *
+     * @param id the id of the note you want to get from storage.
+     * @throws InvalidIdException when id is either below 0, or above integer max value.
+     */
+    private void checkId(int id) throws InvalidIdException {
+        if (id < 0 || id == Integer.MAX_VALUE) {
+            throw new InvalidIdException("id cannot be below 0 or above integer max value");
+        }
     }
 }
